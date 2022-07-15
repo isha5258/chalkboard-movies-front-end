0cf1bc2283eba29f56d816d769abdbe4443fff21
IsaacH <Isaac.hathaway28@gmail.com>
2022-07-15 17:39:36 -0600

    create home link for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,6 +7,10 @@ const NavBar = ({ user, handleLogout }) => {
         <nav>
           <ul>
             <li>Welcome, {user.name}</li>
+            <li> 
+              <Link to='/'> Home
+              </Link>
+            </li>
             <li><Link to="/profiles">Profiles</Link></li>
             <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
             <li><Link to="/changePassword">Change Password</Link></li>
