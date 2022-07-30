5e19d37c573e67f694224060fed74ec9cc2b7cdd
IsaacH <Isaac.hathaway28@gmail.com>
2022-07-30 14:53:29 -0600

    map Popular movies to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import * as apiCalls from '../../services/movieServices.js'
+
 const Home = (props) => {
+  const [popularMovies, setPopularMovies] = useState()
+
+  useEffect(() => {
+    apiCalls.getPopularMovies()
+    .then(popularMovieData => setPopularMovies(popularMovieData))
+  }, [])
+
   return ( 
     <>
       <h1>welcome home</h1>
+      <ul>
+        {popularMovies?.results.map(result => 
+          <li key={result.id}>{result.title}</li>
+        )}
+      </ul>
     </>
   );
 }
--- src/services/movieServices.js
+++ src/services/movieServices.js
@@ -7,5 +7,4 @@ function getPopularMovies() {
 
 export {
   getPopularMovies,
-
 }
